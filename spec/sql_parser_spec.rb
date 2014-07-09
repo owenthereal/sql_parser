0c9f7307c6bbd79afe7144f9d544a2db760346e5
Cameron Martin <cameronmartin123@gmail.com>
2014-07-09 05:44:34 +0100

    Refactored spec to use the new matcher syntax.

--- spec/sql_parser_spec.rb
+++ spec/sql_parser_spec.rb
@@ -7,12 +7,9 @@ shared_context "SqlParser" do
   end
 
   def parse(input)
-    result = @parser.parse(input)
-    if !result
-      puts result.failure_reason
+    @parser.parse(input).tap do |result|
+      puts result.failure_reason unless result
     end
-    result.should be
-    result
   end
 end
 
@@ -20,7 +17,7 @@ describe SqlParser, "parsing" do
   include_context "SqlParser"
   
   it "when conditions are not joined with an :and or :or, does not succeed" do
-    @parser.parse("select first_name from users where first_name='joe' last_name='bob'").should be_nil
+    expect(@parser.parse("select first_name from users where first_name='joe' last_name='bob'")).to be_nil
   end
 end
 
@@ -28,7 +25,7 @@ describe SqlParser, "#tree when parsing select statement" do
   include_context "SqlParser"
 
   it "parses a multi field, table, and where clause statement" do
-    parse("select distinct *, first_name, last_name, middle_name from users, accounts, logins where first_name='joe' and last_name='bob' or age > 25").tree.should == {
+    expect(parse("select distinct *, first_name, last_name, middle_name from users, accounts, logins where first_name='joe' and last_name='bob' or age > 25").tree).to eq({
       :operator => :select,
       :set_quantifier => :distinct,
       :fields => [:'*', :first_name, :last_name, :middle_name],
@@ -40,7 +37,7 @@ describe SqlParser, "#tree when parsing select statement" do
         {:operator => :or},
         {:operator => :'>', :field => :age, :value => 25}
       ]
-    }
+    })
   end
 end
 
@@ -48,7 +45,7 @@ describe SqlParser, "#operator when parsing select statement" do
   include_context "SqlParser"
 
   it "returns :select" do
-    parse("select first_name").operator.should == :select
+    expect(parse("select first_name").operator).to eq(:select)
   end
 end
 
@@ -56,11 +53,11 @@ describe SqlParser, "#set_quantifier when parsing select statement" do
   include_context "SqlParser"
 
   it "when parsing distinct, returns :distinct" do
-    parse("select distinct first_name").set_quantifier.should == :distinct
+    expect(parse("select distinct first_name").set_quantifier).to eq(:distinct)
   end
 
   it "when parsing all, returns :all" do
-    parse("select all first_name").set_quantifier.should == :all
+    expect(parse("select all first_name").set_quantifier).to eq(:all)
   end
 end
 
@@ -68,14 +65,14 @@ describe SqlParser, "#fields when parsing select statement" do
   include_context "SqlParser"
 
   it "returns the fields in the statement" do
-    parse("select first_name").fields.should == [:first_name]
-    parse("select first_name, last_name, middle_name").fields.should == [
+    expect(parse("select first_name").fields).to eq([:first_name])
+    expect(parse("select first_name, last_name, middle_name").fields).to eq([
       :first_name, :last_name, :middle_name
-    ]
+    ])
   end
 
   it "when receiving *, returns * in the fields list" do
-    parse("select *").fields.should == [:'*']
+    expect(parse("select *").fields).to eq([:'*'])
   end
 end
 
@@ -83,40 +80,41 @@ describe SqlParser, "#tables when parsing select statement" do
   include_context "SqlParser"
 
   it "returns tables from the statement" do
-    parse("select first_name from users").tables.should == [:users]
-    parse("select first_name from users, accounts, logins").tables.should == [
+    expect(parse("select first_name from users").tables).to eq([:users])
+    expect(parse("select first_name from users, accounts, logins").tables).to eq([
       :users, :accounts, :logins
-    ]
+    ])
   end
 end
 
 describe SqlParser, "#conditions when parsing select statement" do
   include_context "SqlParser"
   
-  it "when no where conditions, returns empty hash" do
-    parse("select first_name from users").conditions.should == []
+  it "when no where conditions, returns empty array" do
+    expect(parse("select first_name from users").conditions).to eq([])
   end
 
   it "returns equality conditions from the statement" do
-    parse("select first_name from users where id=3").conditions.should == [
+    expect(parse("select first_name from users where id=3").conditions).to eq([
       { :operator => :'=', :field => :id, :value => 3 }
-    ]
-    parse("select first_name from users where first_name='joe'").conditions.should == [
+    ])
+    expect(parse("select first_name from users where first_name='joe'").conditions).to eq([
       { :operator => :'=', :field => :first_name, :value => 'joe' }
-    ]
-    parse("select first_name from users where first_name='joe' and last_name='bob'").conditions.should == [
+    ])
+    
+    expect(parse("select first_name from users where first_name='joe' and last_name='bob'").conditions).to eq([
       {:operator => :'=', :field => :first_name, :value => 'joe'},
       {:operator => :and},
       {:operator => :'=', :field => :last_name, :value => 'bob'}
-    ]
+    ])
   end
 
   it "returns greater than conditions from the statement" do
-    parse("select first_name from users where id>3").conditions.should == [
+    expect(parse("select first_name from users where id>3").conditions).to eq [
       { :operator => :'>', :field => :id, :value => 3 }
     ]
 
-    parse("select first_name from users where id>3 and age>25").conditions.should == [
+    expect(parse("select first_name from users where id>3 and age>25").conditions).to eq [
       {:operator => :'>', :field => :id, :value => 3},
       {:operator => :and},
       {:operator => :'>', :field => :age, :value => 25}
@@ -124,51 +122,51 @@ describe SqlParser, "#conditions when parsing select statement" do
   end
 
   it "returns less than conditions from the statement" do
-    parse("select first_name from users where id<3").conditions.should == [
+    expect(parse("select first_name from users where id<3").conditions).to eq([
       { :operator => :'<', :field => :id, :value => 3 }
-    ]
+    ])
 
-    parse("select first_name from users where id<3 and age<25").conditions.should == [
+    expect(parse("select first_name from users where id<3 and age<25").conditions).to eq([
       {:operator => :'<', :field => :id, :value => 3},
       {:operator => :and},
       {:operator => :'<', :field => :age, :value => 25}
-    ]
+    ])
   end
 
   it "returns greater than or equal to conditions from the statement" do
-    parse("select first_name from users where id>=3").conditions.should == [
+    expect(parse("select first_name from users where id>=3").conditions).to eq([
       { :operator => :'>=', :field => :id, :value => 3 }
-    ]
+    ])
 
-    parse("select first_name from users where id>=3 and age>=25").conditions.should == [
+    expect(parse("select first_name from users where id>=3 and age>=25").conditions).to eq([
       {:operator => :'>=', :field => :id, :value => 3},
       {:operator => :and},
       {:operator => :'>=', :field => :age, :value => 25}
-    ]
+    ])
   end
 
   it "returns less than or equal to conditions from the statement" do
-    parse("select first_name from users where id<=3").conditions.should == [
+    expect(parse("select first_name from users where id<=3").conditions).to eq([
       { :operator => :'<=', :field => :id, :value => 3 }
-    ]
+    ])
 
-    parse("select first_name from users where id<=3 and age<=25").conditions.should == [
+    expect(parse("select first_name from users where id<=3 and age<=25").conditions).to eq([
       {:operator => :'<=', :field => :id, :value => 3},
       {:operator => :and},
       {:operator => :'<=', :field => :age, :value => 25}
-    ]
+    ])
   end
 
   it "returns not equal to conditions from the statement" do
-    parse("select first_name from users where id<>3").conditions.should == [
+    expect(parse("select first_name from users where id<>3").conditions).to eq([
       { :operator => :'<>', :field => :id, :value => 3 }
-    ]
+    ])
 
-    parse("select first_name from users where id<>3 and age<>25").conditions.should == [
+    expect(parse("select first_name from users where id<>3 and age<>25").conditions).to eq([
       {:operator => :'<>', :field => :id, :value => 3},
       {:operator => :and},
       {:operator => :'<>', :field => :age, :value => 25}
-    ]
+    ])
   end
 end
 
@@ -176,21 +174,21 @@ describe SqlParser, "#conditions when parsing select statement with :and operato
   include_context "SqlParser"
 
   it "returns single level :and operation" do
-    parse("select first_name from users where first_name='joe' and last_name='bob'").conditions.should == [
+    expect(parse("select first_name from users where first_name='joe' and last_name='bob'").conditions).to eq([
       {:operator => :'=', :field => :first_name, :value => 'joe'},
       {:operator => :and},
       {:operator => :'=', :field => :last_name, :value => 'bob'}
-    ]
+    ])
   end
 
   it "returns nested :and operations from the statement" do
-    parse("select first_name from users where first_name='joe' and last_name='bob' and middle_name='pat'").conditions.should == [
+    expect(parse("select first_name from users where first_name='joe' and last_name='bob' and middle_name='pat'").conditions).to eq([
       {:operator => :'=', :field => :first_name, :value => 'joe'},
       {:operator => :and},
       {:operator => :'=', :field => :last_name, :value => 'bob'},
       {:operator => :and},
       {:operator => :'=', :field => :middle_name, :value => 'pat'}
-    ]
+    ])
   end
 end
 
@@ -198,21 +196,21 @@ describe SqlParser, "#conditions when parsing select statement with :or operator
   include_context "SqlParser"
 
   it "returns single level :and operation" do
-    parse("select first_name from users where first_name='joe' or last_name='bob'").conditions.should == [
+    expect(parse("select first_name from users where first_name='joe' or last_name='bob'").conditions).to eq([
       {:operator => :'=', :field => :first_name, :value => 'joe'},
       {:operator => :or},
       {:operator => :'=', :field => :last_name, :value => 'bob'}
-    ]
+    ])
   end
 
   it "returns nested :or operations from the statement" do
-    parse("select first_name from users where first_name='joe' or last_name='bob' or middle_name='pat'").conditions.should == [
+    expect(parse("select first_name from users where first_name='joe' or last_name='bob' or middle_name='pat'").conditions).to eq([
       {:operator => :'=', :field => :first_name, :value => 'joe'},
       {:operator => :or},
       {:operator => :'=', :field => :last_name, :value => 'bob'},
       {:operator => :or},
       {:operator => :'=', :field => :middle_name, :value => 'pat'}
-    ]
+    ])
   end
 end
 
@@ -220,12 +218,12 @@ describe SqlParser, "#conditions when parsing select statement with :and and :or
   include_context "SqlParser"
 
   it "returns :and having precedence over :or" do
-    parse("select first_name from users where age > 25 and first_name='joe' or last_name='bob'").conditions.should == [
+    expect(parse("select first_name from users where age > 25 and first_name='joe' or last_name='bob'").conditions).to eq([
       {:operator => :'>', :field => :age, :value => 25},
       {:operator => :and},
       {:operator => :'=', :field => :first_name, :value => 'joe'},
       {:operator => :or},
       {:operator => :'=', :field => :last_name, :value => 'bob'}
-    ]
+    ])
   end
 end
